430dc7abe9f5bbb96ed4c48ae3e1fadb5dc80f5d
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-07-02 08:39:15 +0600

    finish

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 var cors = require("cors");
+app.use(express.json());
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(cors());
@@ -18,7 +19,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    console.log("connected");
+    const tasksCollection = client.db("end_game").collection("tasks");
+
+    // app.get("/task", async (req, res) => {
+    // const query = {};
+    // console.log(query);
+    // const cursor = await tasksCollection.find(query);
+    // const tasks = cursor.toArray();
+    // console.log(tasks);
+    // res.send(tasks);
+    // });
+    app.get("/task", async (req, res) => {
+      const result = await tasksCollection.find({}).toArray();
+      res.send(result);
+    });
+    app.post("/task", async (req, res) => {
+      const text = req.body;
+      const tasks = await tasksCollection.insertOne(text);
+      res.send(tasks);
+    });
   } finally {
   }
 }
